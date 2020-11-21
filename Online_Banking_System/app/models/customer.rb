6935f6b6ff8578311added32537a938720b7f0db
Leticia <leticia.piucco@gmail.com>
2020-11-21 14:33:58 +0000

    Create test for constumer credentials

--- Online_Banking_System/app/models/customer.rb
+++ Online_Banking_System/app/models/customer.rb
@@ -1,4 +1,7 @@
 class Customer < ApplicationRecord
   has_many :Account
   has_secure_password
+  validates :customerNumber, presence: true, length: {minimum: 8, maximum: 16}, uniqueness: true
+  validates :password, presence:true, length: {minimum: 8}
+  validates :email, presence:true, format: {with: URI::MailTo::EMAIL_REGEXP}
 end
