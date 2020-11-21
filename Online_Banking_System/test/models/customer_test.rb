3d9bf515b37be102e0f178e71e50a1fa25c71dee
Leticia <leticia.piucco@gmail.com>
2020-11-21 13:42:13 +0000

    Fix test files

--- Online_Banking_System/test/models/customer_test.rb
+++ Online_Banking_System/test/models/customer_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
+
 class CustomerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @customer = Customer.new({customerNumber: 1234123412341234, email: "john@gmail.com", forename: "john", surname: "smtih", phone: "7777-7777", password: "12345678", password_confirmation: "12345678"})
+  end
+  test "user should be valid" do
+    assert @customer.valid?
+  end
+  test "user with non-matching password should be invalid" do
+    @customer.password = "87654321"
+    assert_not @customer.valid?
+  end
 end
