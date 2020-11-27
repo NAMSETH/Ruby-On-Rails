5f858917284ec6c9763a91b5ed36ba39be10f114
Joshua Harris <joshuakharris12@gmail.com>
2020-11-27 13:32:33 +0000

    Add migration for AdminUsers

--- Online_Banking_System/db/schema.rb
+++ Online_Banking_System/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_27_120310) do
+ActiveRecord::Schema.define(version: 2020_11_27_133059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2020_11_27_120310) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
