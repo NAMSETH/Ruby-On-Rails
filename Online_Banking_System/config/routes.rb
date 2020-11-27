f5222c589bd01245e4cf19258883174e64b25747
Joshua Harris <joshuakharris12@gmail.com>
2020-11-27 12:56:04 +0000

    Comment out old routes

--- Online_Banking_System/config/routes.rb
+++ Online_Banking_System/config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   root 'customers#index'
-  get 'customers/index'
-  get 'customers/show'
-  get 'customers/new'
-  get 'customers/create'
-  get 'customers/update'
-  get 'customers/edit'
-  get 'customers/delete'
-  get 'customers/destroy'
+  # get 'customers/index'
+  # get 'customers/show'
+  # get 'customers/new'
+  # get 'customers/create'
+  # get 'customers/update'
+  # get 'customers/edit'
+  # get 'customers/delete'
+  # get 'customers/destroy'
 
   resources :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
