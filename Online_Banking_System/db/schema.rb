77887473b646bb40a02c0c7485d70c7550a1a6d4
Leticia <leticia.piucco@gmail.com>
2020-11-27 14:08:34 +0000

    Remove wrong admin_user from database and create model

--- Online_Banking_System/db/schema.rb
+++ Online_Banking_System/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_27_120310) do
+ActiveRecord::Schema.define(version: 2020_11_27_140452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2020_11_27_120310) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -37,10 +37,10 @@ ActiveRecord::Schema.define(version: 2020_11_27_120310) do
     t.string "surname"
     t.string "email"
     t.string "phone"
-    t.string "password_digest"
     t.date "dob"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   create_table "transactions", force: :cascade do |t|
