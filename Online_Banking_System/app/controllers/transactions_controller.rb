b5337db3e3a2716d7bff82d9677975142de81c1c
Joshua Harris <joshuakharris12@gmail.com>
2020-12-02 16:40:00 +0000

    Add all transactions to table

--- Online_Banking_System/app/controllers/transactions_controller.rb
+++ Online_Banking_System/app/controllers/transactions_controller.rb
@@ -2,6 +2,6 @@ class TransactionsController < ApplicationController
   def index
     @account = Account.find(params[:account_id])
     id  = @account.id
-    @transactions = Transaction.where(sendingAccount_id: id)
+    @transactions = Transaction.where(sendingAccount_id: id).or(Transaction.where(recievingAccount_id: id))
   end
 end
