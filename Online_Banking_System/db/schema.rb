51861542172b052e70613b8ea16f279230bd1c33
Joshua Harris <joshuakharris12@gmail.com>
2020-12-07 20:03:49 +0000

    Add db migration to delete payments table

--- Online_Banking_System/db/schema.rb
+++ Online_Banking_System/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_165344) do
+ActiveRecord::Schema.define(version: 2020_12_07_200112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,19 +39,10 @@ ActiveRecord::Schema.define(version: 2020_12_02_165344) do
     t.string "surname"
     t.string "email"
     t.string "phone"
-    t.string "password_digest"
     t.date "dob"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "payments", force: :cascade do |t|
-    t.string "recievingAccount"
-    t.string "currency"
-    t.string "sumSended"
-    t.date "paymentDate"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   create_table "transactions", force: :cascade do |t|
