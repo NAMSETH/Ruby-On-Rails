9a71f641cef58c8d56c5009f5693614ea8169938
monjur <monjurmirza@hotmail.co.uk>
2020-12-11 14:34:32 +0000

    qwert

--- Online_Banking_System/app/controllers/customer_session_controller.rb
+++ Online_Banking_System/app/controllers/customer_session_controller.rb
@@ -1,6 +1,8 @@
 class CustomerSessionController < ApplicationController
 before_action :redirect_if_logged_in, only: [:new, :create]
 
+  @@code = ""
+
   def new
   end
 
@@ -8,12 +10,34 @@ before_action :redirect_if_logged_in, only: [:new, :create]
     customer = Customer.find_by(email: params[:session][:email].downcase)
     if customer && customer.authenticate(params[:session][:password])
       log_in(customer)
-      redirect_to(accounts_path)
+      if(customer.verification == true)
+        @@code = code_generate(params[:session][:email])
+        newVerification
+      else
+        redirect_to(accounts_path)
+      end  
     else
       flash.now[:danger] = "No valid credentials were provided"
       render 'new'
     end
   end
+
+  def newVerification
+    render 'verification'
+    
+  end
+
+  def verification
+    code = params[:session][:code]
+    if code == @@code
+      redirect_to(accounts_path)
+    else
+      flash.now[:danger] = "Invalid Code Submitted"
+      render 'verification'
+    end
+  end
+  
+  
 
   def destroy
     log_out
